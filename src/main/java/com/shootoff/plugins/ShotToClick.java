5254aab78d284e2d5617f075d45d9136696e27f8
phrack <code.realm@gmail.com>
2016-03-17 18:17:30 -0400

    Initial commit of Shot to Click

---
+++ src/main/java/com/shootoff/plugins/ShotToClick.java
@@ -0,0 +1,63 @@
+package com.shootoff.plugins;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.shootoff.camera.Shot;
+import com.shootoff.gui.Hit;
+
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
+
+public class ShotToClick extends ProjectorTrainingExerciseBase implements TrainingExercise {
+	private static final Logger logger = LoggerFactory.getLogger(ShotToClick.class);
+
+	private Robot clicker;
+
+	public ShotToClick() {}
+
+	public ShotToClick(List<Group> targets) {
+		super(targets);
+	}
+
+	@Override
+	public void init() {
+		try {
+			clicker = new Robot();
+		} catch (AWTException e) {
+			logger.error("Error initializing Robot to simulate clicks.", e);
+		}
+	}
+
+	@Override
+	public ExerciseMetadata getInfo() {
+		return new ExerciseMetadata("Shot to Click", "1.0", "phrack",
+				"This exercise converts a shot on a projection to a click on the screen being projected. "
+						+ "To use this exercise, start the Projector Arena and calibrate, then minimize the arena "
+						+ "and replace it on the projector with a Flash game or similar that you'd like to play "
+						+ "using your laser trainer. The mouse cursor will move to where a shot is detected and "
+						+ "a left click will be simulated.");
+	}
+
+	@Override
+	public void shotListener(Shot shot, Optional<Hit> hit) {
+		final Point2D arenaScreenOrigin = super.getArenaScreenOrigin();
+
+		System.out.println((int) (shot.getX() + arenaScreenOrigin.getX()) + " " +
+				(int) (shot.getY() + arenaScreenOrigin.getY()));
+		
+		clicker.mouseMove((int) (shot.getX() + arenaScreenOrigin.getX()),
+				(int) (shot.getY() + arenaScreenOrigin.getY()));
+		clicker.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+		clicker.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+	}
+
+	@Override
+	public void reset(List<Group> targets) {}
+}
